0b0d2aa6d70834a6335016ddc0722814dd7f48e4
Adwiteek <sadwiteek@gmail.com>
2026-05-12 06:54:38 +0530

    prod fix

--- controllers/user_auth_controller.py
+++ controllers/user_auth_controller.py
@@ -65,7 +65,7 @@ async def github_callback_controller(code: str):
         value=token,
         httponly=True,
         secure=True,
-        samesite="none",
+        samesite="lax",
         max_age=60 * 60 * 24 * 3
     )
 
